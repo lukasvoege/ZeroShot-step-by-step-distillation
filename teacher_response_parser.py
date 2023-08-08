1ca644e52354d9aa75199b7479c676286a4a4c0c
lukasvoege <lukas.voege@web.de>
2023-08-08 22:29:26 +0200

    fix submodule

--- teacher_response_parser.py
+++ teacher_response_parser.py
@@ -1,1 +1,4 @@
-from distillingStepByStep.data_utils import ESNLIDatasetLoader
+import importlib
+dsbs = importlib.import_module("distilling-step-by-step.data_utils")
+
+dataloader = dsbs.ANLI1DatasetLoader()
