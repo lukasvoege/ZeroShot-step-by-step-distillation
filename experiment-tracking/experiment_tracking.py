83321239e828c857041c9d0f4ba8a6bbeb4a0445
lukasvoege <lukas.voege@web.de>
2023-09-14 20:10:05 +0200

    expereiment tracking

--- experiment-tracking/experiment_tracking.py
+++ experiment-tracking/experiment_tracking.py
@@ -1,39 +1,40 @@
-# %%
 import pandas as pd
 import os
 import json
 
-# dataset | from_pretrained | model_type | llm | subsample | prompt_mix | label_type | alpha | max_input_length | grad_steps*args.batch_size | optimizer_name | lr | run'
-
 # Read csv and have all dtypes as object
-experiments = pd.read_csv("experiment_tracking.csv", sep=";", dtype=object)
+experiments = pd.read_csv("experiment-tracking/experiment_tracking.csv", sep=";", dtype=object)
 # replace nan with "None"
 experiments = experiments.fillna("None")
 
-# %%
+
 def get_params_from_path(path: str):
     params = {}
     path = os.path.normpath(path)
     split = path.split(os.path.sep)
-    params["dataset"] = split[2]
-    params["model"] = split[3]
-    params["mode"] = split[4]
-    params["llm"] = split[5]
-    params["subsample"] = split[6]
-    params["prompt_mix"] = split[7]
-    params["label_type"] = split[8]
-    params["alpha"] = split[9]
-    params["max_input_length"] = split[10]
-    params["batch_size"] = split[11]
-    params["optimizer"] = split[12]
-    params["lr"] = split[13]
-    params["run"] = split[14]
+    params["dataset"] = split[1]
+    params["model"] = split[2]
+    params["mode"] = split[3]
+    params["llm"] = split[4]
+    params["subsample"] = split[5]
+    params["prompt_mix"] = split[6]
+    params["label_type"] = split[7]
+    params["alpha"] = split[8]
+    params["max_input_length"] = split[9]
+    params["batch_size"] = split[10]
+    params["optimizer"] = split[11]
+    params["lr"] = split[12]
+    params["run"] = split[13]
 
     params["id"] = get_identifier(params)
 
     category = "step-by-step " if params["mode"] == "task_prefix" else "standard "
-    category += "distillation" if params["label_type"] == "llm" else "finetuning"
+    category += "Distillation" if params["label_type"] == "llm" else "Finetuning"
     params["category"] = category
+
+    params["train_duration"] = "None"
+    params["eval_acc"] = "None"
+    params["test_acc"] = "None"
 
     return params
 
@@ -60,9 +61,7 @@ def get_identifier(params: dict):
 # add id column
 experiments["id"] = experiments.apply(lambda row: get_identifier(row.to_dict()), axis=1)
 
-# %%
-
-for root, dirs, files in os.walk("../results"):
+for root, dirs, files in os.walk("results"):
     if files:
         params = get_params_from_path(root)
         for file in files:
@@ -85,8 +84,11 @@ for root, dirs, files in os.walk("../results"):
             experiments.loc[experiments["id"] == params["id"], "train_duration"] = params["train_duration"]
             experiments.loc[experiments["id"] == params["id"], "eval_acc"] = params["eval_acc"]
             experiments.loc[experiments["id"] == params["id"], "test_acc"] = params["test_acc"]
+            print(f"updated {params['id']}")
         else:
             # concat to experiments
             experiments = pd.concat([experiments, pd.DataFrame(params, index=[0])])
+            print(f"added {params['id']}")
 
-# %%
+# save experiments to csv again
+experiments.drop("id", axis=1).to_csv("experiment-tracking/experiment_tracking.csv", sep=";", index=False)
