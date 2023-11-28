86fd85b9a8c354c67f2ecb4d67797059b2ed24e7
lukasvoege <lukas.voege@web.de>
2023-11-28 12:10:08 +0100

    new Xpl erxperiments

--- experiment-tracking/experiment_tracking.py
+++ experiment-tracking/experiment_tracking.py
@@ -3,7 +3,7 @@ import os
 import json
 import argparse
 
-DEFAULT_PASSTHROUGH = "--bf16 --tf32 --es_threshold 0.0001 --es_patience 30 --parallelize --no_save"
+DEFAULT_PASSTHROUGH = "--bf16 --tf32 --es_threshold 0.0001 --es_patience 20 --parallelize --no_save"
 
 def get_experiments_df():
     # Read csv and have all dtypes as object
--- experiment-tracking/scripts/large.sh
+++ experiment-tracking/scripts/large.sh
@@ -1,32 +1,32 @@
 export TOKENIZERS_PARALLELISM=true
 
-# (Model Size ANLI1 GPT35) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - gpt35 llm - 6 prompt_mix - 1.0 subsample - 0.5 alpha
+# (LONG X LRG GPT35 LABEL GIVEN) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - palm llm - 0 prompt_mix - 0.5 subsample - 0.5 alpha
 echo '##################################################'
-echo '(Model Size ANLI1 GPT35) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - gpt35 llm - 6 prompt_mix - 1.0 subsample - 0.5 alpha'
-python distilling-step-by-step/run.py --from_pretrained google/t5-v1_1-large --dataset anli1 --model_type task_prefix --label_type gt --llm gpt35 --prompt_mix 6 --subsample 1.0 --alpha 0.5 --batch_size 8 --bf16 --tf32 --es_threshold 0.0001 --es_patience 30 --parallelize --no_save
+echo '(LONG X LRG GPT35 LABEL GIVEN) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - palm llm - 0 prompt_mix - 0.5 subsample - 0.5 alpha'
+python distilling-step-by-step/run.py --from_pretrained google/t5-v1_1-large --dataset anli1 --model_type task_prefix --label_type gt --llm palm --subsample 0.5 --alpha 0.5 --batch_size 8 --bf16 --tf32 --es_threshold 0.0001 --es_patience 20 --parallelize --no_save
 
-# (RERUN Model Size ANLI1 GPT35) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - gpt35 llm - 6 prompt_mix - 1.0 subsample - 0.5 alpha
+# (RERUN LONG X LRG ANLI1 GPT35 LABEL GIVEN) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - palm llm - 0 prompt_mix - 0.5 subsample - 0.5 alpha
 echo '##################################################'
-echo '(RERUN Model Size ANLI1 GPT35) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - gpt35 llm - 6 prompt_mix - 1.0 subsample - 0.5 alpha'
-python distilling-step-by-step/run.py --from_pretrained google/t5-v1_1-large --dataset anli1 --model_type task_prefix --label_type gt --llm gpt35 --prompt_mix 6 --subsample 1.0 --alpha 0.5 --batch_size 8 --run 1 --bf16 --tf32 --es_threshold 0.0001 --es_patience 30 --parallelize --no_save
+echo '(RERUN LONG X LRG ANLI1 GPT35 LABEL GIVEN) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - palm llm - 0 prompt_mix - 0.5 subsample - 0.5 alpha'
+python distilling-step-by-step/run.py --from_pretrained google/t5-v1_1-large --dataset anli1 --model_type task_prefix --label_type gt --llm palm --subsample 0.5 --alpha 0.5 --batch_size 8 --run 1 --bf16 --tf32 --es_threshold 0.0001 --es_patience 20 --parallelize --no_save
 
-# (RERUN Model Size ANLI1 GPT35) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - gpt35 llm - 6 prompt_mix - 1.0 subsample - 0.5 alpha
+# (RERUN LONG X LRG ANLI1 GPT35 LABEL GIVEN) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - palm llm - 0 prompt_mix - 0.5 subsample - 0.5 alpha
 echo '##################################################'
-echo '(RERUN Model Size ANLI1 GPT35) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - gpt35 llm - 6 prompt_mix - 1.0 subsample - 0.5 alpha'
-python distilling-step-by-step/run.py --from_pretrained google/t5-v1_1-large --dataset anli1 --model_type task_prefix --label_type gt --llm gpt35 --prompt_mix 6 --subsample 1.0 --alpha 0.5 --batch_size 8 --run 2 --bf16 --tf32 --es_threshold 0.0001 --es_patience 30 --parallelize --no_save
+echo '(RERUN LONG X LRG ANLI1 GPT35 LABEL GIVEN) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - palm llm - 0 prompt_mix - 0.5 subsample - 0.5 alpha'
+python distilling-step-by-step/run.py --from_pretrained google/t5-v1_1-large --dataset anli1 --model_type task_prefix --label_type gt --llm palm --subsample 0.5 --alpha 0.5 --batch_size 8 --run 2 --bf16 --tf32 --es_threshold 0.0001 --es_patience 20 --parallelize --no_save
 
-# (Model Size ANLI1 GPT35) step-by-step Distillation: anli1 - t5-v1_1-large - task_prefix - llm label_type - gpt35 llm - 6 prompt_mix - 1.0 subsample - 0.5 alpha
+# (LONG X LRG GPT35 LABEL GIVEN) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - gpt35 llm - 6 prompt_mix - 0.5 subsample - 0.5 alpha
 echo '##################################################'
-echo '(Model Size ANLI1 GPT35) step-by-step Distillation: anli1 - t5-v1_1-large - task_prefix - llm label_type - gpt35 llm - 6 prompt_mix - 1.0 subsample - 0.5 alpha'
-python distilling-step-by-step/run.py --from_pretrained google/t5-v1_1-large --dataset anli1 --model_type task_prefix --label_type llm --llm gpt35 --prompt_mix 6 --subsample 1.0 --alpha 0.5 --batch_size 8 --bf16 --tf32 --es_threshold 0.0001 --es_patience 30 --parallelize --no_save
+echo '(LONG X LRG GPT35 LABEL GIVEN) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - gpt35 llm - 6 prompt_mix - 0.5 subsample - 0.5 alpha'
+python distilling-step-by-step/run.py --from_pretrained google/t5-v1_1-large --dataset anli1 --model_type task_prefix --label_type gt --llm gpt35 --prompt_mix 6 --subsample 0.5 --alpha 0.5 --batch_size 8 --bf16 --tf32 --es_threshold 0.0001 --es_patience 20 --parallelize --no_save
 
-# (RERUN Model Size ANLI1 GPT35) step-by-step Distillation: anli1 - t5-v1_1-large - task_prefix - llm label_type - gpt35 llm - 6 prompt_mix - 1.0 subsample - 0.5 alpha
+# (RERUN LONG X LRG ANLI1 GPT35 LABEL GIVEN) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - gpt35 llm - 6 prompt_mix - 0.5 subsample - 0.5 alpha
 echo '##################################################'
-echo '(RERUN Model Size ANLI1 GPT35) step-by-step Distillation: anli1 - t5-v1_1-large - task_prefix - llm label_type - gpt35 llm - 6 prompt_mix - 1.0 subsample - 0.5 alpha'
-python distilling-step-by-step/run.py --from_pretrained google/t5-v1_1-large --dataset anli1 --model_type task_prefix --label_type llm --llm gpt35 --prompt_mix 6 --subsample 1.0 --alpha 0.5 --batch_size 8 --run 1 --bf16 --tf32 --es_threshold 0.0001 --es_patience 30 --parallelize --no_save
+echo '(RERUN LONG X LRG ANLI1 GPT35 LABEL GIVEN) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - gpt35 llm - 6 prompt_mix - 0.5 subsample - 0.5 alpha'
+python distilling-step-by-step/run.py --from_pretrained google/t5-v1_1-large --dataset anli1 --model_type task_prefix --label_type gt --llm gpt35 --prompt_mix 6 --subsample 0.5 --alpha 0.5 --batch_size 8 --run 1 --bf16 --tf32 --es_threshold 0.0001 --es_patience 20 --parallelize --no_save
 
-# (RERUN Model Size ANLI1 GPT35) step-by-step Distillation: anli1 - t5-v1_1-large - task_prefix - llm label_type - gpt35 llm - 6 prompt_mix - 1.0 subsample - 0.5 alpha
+# (RERUN LONG X LRG ANLI1 GPT35 LABEL GIVEN) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - gpt35 llm - 6 prompt_mix - 0.5 subsample - 0.5 alpha
 echo '##################################################'
-echo '(RERUN Model Size ANLI1 GPT35) step-by-step Distillation: anli1 - t5-v1_1-large - task_prefix - llm label_type - gpt35 llm - 6 prompt_mix - 1.0 subsample - 0.5 alpha'
-python distilling-step-by-step/run.py --from_pretrained google/t5-v1_1-large --dataset anli1 --model_type task_prefix --label_type llm --llm gpt35 --prompt_mix 6 --subsample 1.0 --alpha 0.5 --batch_size 8 --run 2 --bf16 --tf32 --es_threshold 0.0001 --es_patience 30 --parallelize --no_save
+echo '(RERUN LONG X LRG ANLI1 GPT35 LABEL GIVEN) step-by-step Finetuning: anli1 - t5-v1_1-large - task_prefix - gt label_type - gpt35 llm - 6 prompt_mix - 0.5 subsample - 0.5 alpha'
+python distilling-step-by-step/run.py --from_pretrained google/t5-v1_1-large --dataset anli1 --model_type task_prefix --label_type gt --llm gpt35 --prompt_mix 6 --subsample 0.5 --alpha 0.5 --batch_size 8 --run 2 --bf16 --tf32 --es_threshold 0.0001 --es_patience 20 --parallelize --no_save
 
